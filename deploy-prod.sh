b3284310b408ed66dcc8326f4bfd127b84d4e971
savamane <savamane@noreply.users.github.com>
2019-09-18 19:56:26 +0300

    deploy to S3 and create CF invalidation

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,8 +2,10 @@ rm -rf ./build
 
 yarn build
 
-echo "oasis.app/trade" > ./build/CNAME
+#echo "oasis.app/trade" > ./build/CNAME
 
 cp ./build/200.html ./build/404.html
 
 gh-pages -d ./build
+aws s3 sync ./build s3://$AWS_BUCKET_NAME/trade/ --delete
+aws cloudfront create-invalidation --distribution-id $AWS_CF_ID --paths /*
