e0a45b175a9dd744840490973e5191aa24a272a4
John Doe <georgi.h.georgieff@gmail.com>
2019-09-18 18:36:36 +0200

    Apply configuration for production

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,9 +1,3 @@
 rm -rf ./build
 
-yarn build
-
-echo "oasis.app/trade" > ./build/CNAME
-
-cp ./build/200.html ./build/404.html
-
-gh-pages -d ./build
+PUBLIC_URL='https://oasis.app/trade' yarn build
