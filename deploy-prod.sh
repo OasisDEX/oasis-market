3154a8e74675a9992e75028be03018ebb82770c7
John Doe <georgi.h.georgieff@gmail.com>
2019-09-18 19:59:54 +0200

    Fix deployment script

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,8 +2,20 @@ rm -rf ./build
 
 PUBLIC_URL='https://oasis.app/trade' yarn build
 
+# Push to ghpages branch
 cp ./build/200.html ./build/404.html
-
 gh-pages -d ./build
+
+# Deploy to AWS
+sudo apt update
+sudo apt install python-pip3
+pip3 install --upgrade --user awscli
+export PATH=~/.local/bin:$PATH
+
+aws configure set default.region $AWS_REGION
+aws configure set default.output json
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
 aws s3 sync ./build s3://$AWS_BUCKET_NAME/trade/ --delete
 aws cloudfront create-invalidation --distribution-id $AWS_CF_ID --paths /*
