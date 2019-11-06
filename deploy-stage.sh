f7bd459c205efeb433a650951efca0698f0ed0f5
John Doe <georgi.h.georgieff@gmail.com>
2019-11-06 17:07:16 +0100

    Setup staging CI/CD configuration

---
+++ deploy-stage.sh
@@ -0,0 +1,21 @@
+rm -rf ./build
+
+PUBLIC_URL='https://staging.oasis.app/trade' yarn build
+
+# Push to ghpages branch
+cp ./build/200.html ./build/404.html
+gh-pages -d ./build
+
+# Deploy to AWS
+sudo apt update
+sudo apt install python-pip
+pip install --upgrade --user awscli
+export PATH=$HOME/.local/bin:$PATH
+
+aws configure set default.region $AWS_REGION
+aws configure set default.output json
+aws configure set aws_access_key_id $STAGE_AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $STAGE_AWS_SECRET_ACCESS_KEY
+
+aws s3 sync ./build s3://$STAGE_AWS_BUCKET_NAME/trade/ --delete
+aws cloudfront create-invalidation --distribution-id $STAGE_AWS_CF_ID --paths /*
