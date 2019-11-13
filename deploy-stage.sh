89ee7b2fa091997c94a9b78a1b5a7896da4d9174
John Doe <georgi.h.georgieff@gmail.com>
2019-11-13 19:23:14 +0100

    Some improvements

--- deploy-stage.sh
+++ deploy-stage.sh
@@ -18,4 +18,4 @@ aws configure set aws_access_key_id $STAGE_AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $STAGE_AWS_SECRET_ACCESS_KEY
 
 aws s3 sync ./build s3://$STAGE_AWS_BUCKET_NAME/trade/ --delete
-aws cloudfront create-invalidation --distribution-id $STAGE_AWS_CF_ID --paths /*
+aws cloudfront create-invalidation --distribution-id $STAGE_AWS_CF_ID --paths "/*"
