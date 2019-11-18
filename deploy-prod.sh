59a50ff12aaa6e20125cc901ced69c9bf234742e
John Doe <georgi.h.georgieff@gmail.com>
2019-11-18 15:21:49 +0100

    Revert changes done in deploy-prod. AWS params will be set correctly as CI variables.

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,7 +1,6 @@
 rm -rf ./build
 
-# MAKE THIS TO OASIS.APP
-PUBLIC_URL='https://staging.oasis.app/trade' yarn build
+PUBLIC_URL='https://oasis.app/trade' yarn build
 
 # Push to ghpages branch
 cp ./build/200.html ./build/404.html
@@ -15,9 +14,9 @@ export PATH=$HOME/.local/bin:$PATH
 
 aws configure set default.region $AWS_REGION
 aws configure set default.output json
-aws configure set aws_access_key_id $STAGE_AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $STAGE_AWS_SECRET_ACCESS_KEY
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
-aws s3 sync ./build s3://$STAGE_AWS_BUCKET_NAME/trade/ --delete
-aws cloudfront create-invalidation --distribution-id $STAGE_AWS_CF_ID --paths "/*"
+aws s3 sync ./build s3://$AWS_BUCKET_NAME/trade/ --delete
+aws cloudfront create-invalidation --distribution-id $AWS_CF_ID --paths "/*"
 
