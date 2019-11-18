e84b82bb2be12c32c8e5bfdb8be864b3b0e3e04e
Maciek Kamiński <aundumla@gmail.com>
2019-11-18 12:50:12 +0100

    DAI pairs enabled

--- src/blockchain/config.ts
+++ src/blockchain/config.ts
@@ -41,16 +41,16 @@ import * as saiTub from './abi/sai-tub.abi.json';
 import { web3 } from './web3';
 
 export const tradingPairs: TradingPair[] = [
-  // { base: 'WETH', quote: 'DAI' },
-  //   { base: 'REP', quote: 'DAI' },
-  //   { base: 'ZRX', quote: 'DAI' },
-  //   { base: 'BAT', quote: 'DAI' },
+  { base: 'WETH', quote: 'DAI' },
+    { base: 'REP', quote: 'DAI' },
+    { base: 'ZRX', quote: 'DAI' },
+    { base: 'BAT', quote: 'DAI' },
     { base: 'WETH', quote: 'SAI' },
     { base: 'REP', quote: 'SAI' },
     { base: 'ZRX', quote: 'SAI' },
     { base: 'BAT', quote: 'SAI' },
     { base: 'SAI', quote: 'USDC' },
-    // { base: 'DAI', quote: 'USDC' },
+    { base: 'DAI', quote: 'USDC' },
     { base: 'REP', quote: 'WETH' },
     { base: 'ZRX', quote: 'WETH' },
     { base: 'BAT', quote: 'WETH' },
