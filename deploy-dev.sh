3a6eda669112b731ebd84c0880eea47807448ab3
John <bulk.yard@protonmail.com>
2020-02-03 16:28:29 +0100

    Change folder

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,6 @@
 rm -rf ./build
 
-PUBLIC_URL='https://staging.oasis.app/old-trade' yarn build
+PUBLIC_URL='https://staging.oasis.app//expired/trade' yarn build
 
 # Deploy to AWS
 sudo apt update
@@ -13,6 +13,6 @@ aws configure set default.output json
 aws configure set aws_access_key_id $STAGE_AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $STAGE_AWS_SECRET_ACCESS_KEY
 
-aws s3 sync ./build s3://$STAGE_AWS_BUCKET_NAME/old-trade/ --delete
+aws s3 sync ./build s3://$STAGE_AWS_BUCKET_NAME/expired/trade/ --delete
 aws cloudfront create-invalidation --distribution-id $STAGE_AWS_CF_ID --paths "/*"
 
